9b13e149bdda99004e3308a8160846bca364f7b7
Federico Rossendy <fede07@users.noreply.github.com>
2023-10-09 23:23:40 -0300

    Completed Project

--- main.py
+++ main.py
@@ -8,7 +8,32 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+score = Scoreboard()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkeypress(player.moveUp, "w")
+screen.onkeypress(player.moveDown, "s")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    #detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20 :
+            score.gameover()
+            game_is_on = False
+
+    #detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        score.increase_level()
+
+screen.exitonclick()
+
--- player.py
+++ player.py
@@ -1,7 +1,33 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    
+    def __init__(self) -> None:
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.shapesize(stretch_len=1, stretch_wid=1)
+        self.color("green")
+        self.setheading(90)
+        self.go_to_start()
+
+    def moveUp(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def moveDown(self):
+        new_y = self.ycor() - MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+    
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        return False
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
